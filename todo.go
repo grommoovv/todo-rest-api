e9065830fd7adf2eef319248cbdf1d8c7f5f9fd5
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-30 19:56:15 +0300

    feat(server): add todo structs

---
+++ todo.go
@@ -0,0 +1,26 @@
+package todo
+
+type TodoItem struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+type TodoList struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type UserList struct {
+	Id     int
+	UserId int
+	ListId int
+}
+
+type ListItem struct {
+	Id     int
+	ListId int
+	ItemId int
+}
