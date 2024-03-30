a8cb137fa42d4c0930e5a05d41da62f3274d7562
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-30 19:58:40 +0300

    feat(server): init handlers & impl run server logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	todo "todo-rest-api"
+	"todo-rest-api/internal/handler"
+)
+
 func main() {
 
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
