955d6566738e1c7aa61c94b18aa0f60943991059
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-30 20:20:37 +0300

    feat(server): inject dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	todo "todo-rest-api"
 	"todo-rest-api/internal/handler"
+	"todo-rest-api/internal/repository"
+	"todo-rest-api/internal/service"
 )
 
 func main() {
-
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 
