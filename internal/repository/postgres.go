d335d9819707144ba41c8e2b226e72f2a1de739d
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-31 19:29:53 +0300

    fix(server): add "s"

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	usersTable      = "users"
-	todoListTable   = "todo_lists"
+	todoListsTable  = "todo_lists"
 	usersListsTable = "users_lists"
 	todoItemsTable  = "todo_items"
 	listsItemsTable = "lists_items"
