68d58ca10887ccb93cad4264f1290d7005103d18
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-31 20:22:02 +0300

    feat(server): impl new logic

--- todo.go
+++ todo.go
@@ -2,26 +2,26 @@ package todo
 
 import "errors"
 
-type TodoItem struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title" binding:"required"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
-}
-
 type TodoList struct {
 	Id          int    `json:"id" db:"id"`
 	Title       string `json:"title" db:"title" binding:"required"`
 	Description string `json:"description" db:"description"`
 }
 
-type UserList struct {
+type UsersList struct {
 	Id     int
 	UserId int
 	ListId int
 }
 
-type ListItem struct {
+type TodoItem struct {
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
+	Done        bool   `json:"done" db:"done"`
+}
+
+type ListsItem struct {
 	Id     int
 	ListId int
 	ItemId int
@@ -32,8 +32,22 @@ type UpdateListInput struct {
 	Description *string `json:"description"`
 }
 
-func (u UpdateListInput) Validate() error {
-	if u.Title == nil && u.Description == nil {
+func (i UpdateListInput) Validate() error {
+	if i.Title == nil && i.Description == nil {
+		return errors.New("update structure has no values")
+	}
+
+	return nil
+}
+
+type UpdateItemInput struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	Done        *bool   `json:"done"`
+}
+
+func (i UpdateItemInput) Validate() error {
+	if i.Title == nil && i.Description == nil && i.Done == nil {
 		return errors.New("update structure has no values")
 	}
 
