45b3ca37c7d2e4a9aaf7cbe671030add5372bc50
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-31 17:25:08 +0300

    feat(server): impl signIn func

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	todo "todo-rest-api"
@@ -24,9 +25,33 @@ func (h *Handler) singUp(c *gin.Context) {
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
+}
 
+type singInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 func (h *Handler) singIn(c *gin.Context) {
+	var input singInInput
 
+	fmt.Println("username", input.Username, "password", input.Password)
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Println("username", input.Username, "password", input.Password)
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
