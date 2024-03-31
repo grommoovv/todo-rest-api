32a2d2fc99345a934b2af0c986769bd1af76c7da
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-31 20:26:41 +0300

    refactor(server): add protected handler group

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -16,29 +16,32 @@ func NewHandler(services *service.Service) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
-	auth := router.Group("/auth")
+	api := router.Group("/api/v1")
 	{
-		auth.POST("/sign-up", h.singUp)
-		auth.POST("/sign-in", h.singIn)
-	}
-
-	api := router.Group("/api/v1", h.userIdentity)
-	{
-		lists := api.Group("/lists")
+		auth := api.Group("/auth")
 		{
-			lists.POST("/", h.createList)
-			lists.GET("/", h.getAllLists)
-			lists.GET("/:id", h.getListById)
-			lists.PUT("/:id", h.updateList)
-			lists.DELETE("/:id", h.deleteList)
+			auth.POST("/sign-up", h.singUp)
+			auth.POST("/sign-in", h.singIn)
+		}
 
-			items := lists.Group(":id/items")
+		protected := api.Group("", h.userIdentity)
+		{
+			lists := protected.Group("/lists")
 			{
-				items.POST("/", h.createItem)
-				items.GET("/", h.getAllItems)
-				items.GET("/:item_id", h.getItemById)
-				items.PUT("/:item_id", h.updateItem)
-				items.DELETE("/:item_id", h.deleteItem)
+				lists.POST("/", h.createList)
+				lists.GET("/", h.getAllLists)
+				lists.GET("/:id", h.getListById)
+				lists.PUT("/:id", h.updateList)
+				lists.DELETE("/:id", h.deleteList)
+
+				items := lists.Group(":id/items")
+				{
+					items.POST("/", h.createItem)
+					items.GET("/", h.getAllItems)
+					items.GET("/:item_id", h.getItemById)
+					items.PUT("/:item_id", h.updateItem)
+					items.DELETE("/:item_id", h.deleteItem)
+				}
 			}
 		}
 	}
