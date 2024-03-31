0f8c43e1c97464c25964c727457ba3e35602dc90
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-03-31 19:31:10 +0300

    feat(server): impl TodoListService & its funcs

---
+++ internal/service/list.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	todo "todo-rest-api"
+	"todo-rest-api/internal/repository"
+)
+
+type TodoListService struct {
+	repo repository.TodoList
+}
+
+func NewTodoListService(repo repository.TodoList) *TodoListService {
+	return &TodoListService{repo: repo}
+}
+
+func (t *TodoListService) Create(userId int, list todo.TodoList) (int, error) {
+	return t.repo.Create(userId, list)
+}
+
+func (t *TodoListService) GetAll(userId int) ([]todo.TodoList, error) {
+	return t.repo.GetAll(userId)
+
+}
+
+func (t *TodoListService) GetById(userId, listId int) (todo.TodoList, error) {
+	return t.repo.GetById(userId, listId)
+}
+
+func (t *TodoListService) Delete(userId, listId int) error {
+	return t.repo.Delete(userId, listId)
+}
+
+func (t *TodoListService) Update(userId, listId int, input todo.UpdateListInput) error {
+	if err := input.Validate(); err != nil {
+		return err
+	}
+
+	return t.repo.Update(userId, listId, input)
+}
